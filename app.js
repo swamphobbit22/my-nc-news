33546d0d5f1dce723470ec3353bbe1a51d7cee5e
Kirsty <congokirsty@gmail.com>
2024-11-25 15:12:02 +0000

    completes tasks 3 and 4

--- app.js
+++ app.js
@@ -1,10 +1,49 @@
 const express = require('express');
 const app = express();
-const endpointsJson = require('./endpoints.json')
-//const getApi = require('./controllers/api-controller')
+const endpointsJson = require('./endpoints.json');
+const { getTopics , getSingleArticle} = require('./controllers/api-controller');
 
 app.get('/api', (req, res) => {
     res.status(200).send({ endpoints: endpointsJson})
 })
+
+app.get('/api/topics', getTopics);
+
+app.get('/api/articles/:article_id', getSingleArticle)
+
+
+
+
+
+// Error handling middleware starts here...
+
+app.use((err, req, res, next) => {
+    if(err.status){
+      res.status(404).send({msg: "Does not exist"})
+    } else {
+      next(err);
+    }
+  })
+  
+  app.use((err, req, res, next) => {
+    if(err.status){
+      res.status(204).send({msg: "No Content"})
+    } else {
+      next(err);
+    }
+  })
+  
+  app.use((err, req, res, next) => {
+      res.status(400).send({ msg: "Bad request"})
+  })
+
+  app.use((err, req, res, next) => {
+    if(err.status){
+        res.status(err.status).send({ msg: err.msg});
+    } else {
+        res.status(500).send({ msg: 'Internal Server Error'});
+    }
+  })
+  
 
 module.exports = app;
