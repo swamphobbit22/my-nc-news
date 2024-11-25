e5bdc295cc1a538aab9fe16ae421751487d2859c
Kirsty <congokirsty@gmail.com>
2024-11-25 17:41:23 +0000

    partially completed task 5 - still have some testing to complete

--- models/api-models.js
+++ models/api-models.js
@@ -1,10 +1,8 @@
-const { promiseHooks } = require('v8');
 const db = require('../db/connection')
 
 exports.readTopics = () => {
     return db.query(`SELECT * FROM topics;`)
     .then(({ rows }) => {
-        console.log(rows)
         return rows;
     })
 }
@@ -19,10 +17,32 @@ exports.readSingleArticle = (article_id) => {
         if(rows.length === 0) {
             return Promise.reject({ status: 404, msg: 'Article not found'})
         } 
-        console.log(rows, '<<< rows')
        return rows[0]
     })
     .catch((err) => {
         return err;
     })
+}
+
+exports.readAllArticles = () => {
+    return db.query(`
+        SELECT 
+        articles.author, 
+        articles.title, 
+        articles.article_id, 
+        articles.topic, 
+        articles.created_at, 
+        articles.votes, 
+        articles.article_img_url,
+        COUNT(comments.comment_id)::INTEGER AS comment_count
+        FROM articles
+        LEFT JOIN comments
+        ON comments.article_id = articles.article_id
+        GROUP BY articles.article_id
+        ORDER BY articles.created_at DESC;
+        `)
+        .then(({ rows }) => {
+            //console.log(rows, '<<< rows from model')
+            return rows;
+        })
 }
