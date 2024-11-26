cb970a795ab4784bf13b8f62c1b81398a683cf06
Kirsty <congokirsty@gmail.com>
2024-11-26 11:00:43 +0000

    adds error handling

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const endpointsJson = require('./endpoints.json');
 const { getTopics , getSingleArticle, getAllArticles} = require('./controllers/api-controller');
+const { pgErrorhandler, customErrorhandler, serverErrorhandler } = require('./errors/error-handling');
+
+
 
 app.get('/api', (req, res) => {
     res.status(200).send({ endpoints: endpointsJson})
@@ -17,35 +20,42 @@ app.get('/api/articles', getAllArticles)
 
 
 
-// Error handling middleware starts here...
+// Error handling
+app.use(pgErrorhandler);
 
-app.use((err, req, res, next) => {
-    if(err.status){
-      res.status(404).send({msg: "Does not exist"})
-    } else {
-      next(err);
-    }
-  })
-  
-  app.use((err, req, res, next) => {
-    if(err.status){
-      res.status(204).send({msg: "No Content"})
-    } else {
-      next(err);
-    }
-  })
-  
-  app.use((err, req, res, next) => {
-      res.status(400).send({ msg: "Bad request"})
-  })
+app.use(customErrorhandler);
 
-  app.use((err, req, res, next) => {
-    if(err.status){
-        res.status(err.status).send({ msg: err.msg});
-    } else {
-        res.status(500).send({ msg: 'Internal Server Error'});
-    }
-  })
+app.use(serverErrorhandler)
+
+
+
+// app.use((err, req, res, next) => {
+//     if(err.status){
+//       res.status(404).send({msg: "Does not exist"})
+//     } else {
+//       next(err);
+//     }
+//   })
+  
+//   app.use((err, req, res, next) => {
+//     if(err.status){
+//       res.status(204).send({msg: "No Content"})
+//     } else {
+//       next(err);
+//     }
+//   })
+  
+//   app.use((err, req, res, next) => {
+//       res.status(400).send({ msg: "Bad request"})
+//   })
+
+//   app.use((err, req, res, next) => {
+//     if(err.status){
+//         res.status(err.status).send({ msg: err.msg});
+//     } else {
+//         res.status(500).send({ msg: 'Internal Server Error'});
+//     }
+//   })
   
 
 module.exports = app;
