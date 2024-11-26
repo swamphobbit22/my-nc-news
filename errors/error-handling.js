8da949c19974984fcb16a7ed30557a5842eff106
Kirsty <congokirsty@gmail.com>
2024-11-26 10:58:03 +0000

    completes error handling in separate file

---
+++ errors/error-handling.js
@@ -0,0 +1,16 @@
+exports.pgErrorhandler = (err, req, res, next) => {
+    if (err.code === "22P02") {
+        res.status(400).send({ msg: "Invalid input" });
+      } else next(err);
+};
+
+exports.customErrorhandler = (err, req, res, next) => {
+    if (err.status) {
+        res.status(err.status).send({ msg: err.msg });
+      } else next(err);
+};
+
+exports.serverErrorhandler = (err, req, res, next) => {
+    console.log(err);
+    res.status(500).send({ msg: "Internal Server Error" });
+};
