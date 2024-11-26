ea9f25e272c521f15a0246848a0c10d14248fb4d
Kirsty <congokirsty@gmail.com>
2024-11-26 14:29:19 +0000

    completes tasks 5 and 6

--- models/api-models.js
+++ models/api-models.js
@@ -43,7 +43,28 @@ exports.readAllArticles = () => {
         ORDER BY articles.created_at DESC;
         `)
         .then(({ rows }) => {
-           
             return rows;
         })
+}
+
+
+exports.readCommentsByArticleId = (article_id) => {
+    return db.query(`
+        SELECT 
+            comment_id, 
+            votes, 
+            created_at, 
+            author, 
+            body, 
+            article_id 
+        FROM comments
+        WHERE article_id = $1
+        ORDER BY created_at DESC 
+        `, [article_id]) 
+    .then (({ rows }) => {
+         if(rows.length === 0) {
+             return Promise.reject({ status: 404, msg: 'article not found'})
+         } 
+         return rows;
+    })
 }
