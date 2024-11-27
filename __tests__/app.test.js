f1c5ed79ce6daffc840d76eb3a4415fe089d9487
Kirsty <congokirsty@gmail.com>
2024-11-27 11:10:22 +0000

    completes testing for task 7

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,6 +8,7 @@ const request = require('supertest');
 
 
 
+
 /* Set up your test imports here */
 
 /* Set up your beforeEach & afterAll functions here */
@@ -85,17 +86,6 @@ it('should return an article with a specific id ', () => {
         expect(msg).toBe('article not found')
       })
     });
-
-    // it.only('it should return created_at field in the correct format', () => {
-    //   return request(app)
-    //   .get('/api/articles/2')
-    //   .expect(200)
-    //   .then(({ body }) => {
-    //     const article = body.articles;
-    //     console.log(article.created_at)
-    //     expect(new Date(article.created_at).toString()).not.toBe('Invalid Date')
-    //   })
-    // })
 });
 
 describe('get /api/articles', () => {
@@ -190,7 +180,58 @@ describe('POST /api/articles/:article_id/comments', () => {
     })
   })
 
-  // it.skip('should return a 400 error code when field are empty', () => {
+  it('400 - should respond with 400 error with an invalid input - article id', () => {
+    const addNewComment = {username: 'lurker', body: 'Scooby Scooby Doo, where are you? Hey, Scooby, leave those cats alone! All in all you are just another dog with a bone!'}
+    return request(app)
+    .post('/api/articles/banana/comments')
+    .send(addNewComment)
+    .expect(400)
+    .then(({body}) => {
+      const { msg } = body;
+      expect(msg).toBe('invalid input');
+    })
+  })
+
+  it('404 - should respond with 404 error with an id that does not exist in the database', () => {
+    const addNewComment = {username: 'lurker', body: 'Scooby Scooby Doo, where are you? Hey, Scooby, leave those cats alone! All in all you are just another dog with a bone!'}
+    return request(app)
+    .post('/api/articles/999/comments')
+    .send(addNewComment)
+    .expect(404)
+    .then(({body}) => {
+      const { msg } = body;
+      expect(msg).toBe('article not found');
+    })
+  })
+})
+
+describe('PATCH /api/articles/:article_id', () => {
+  it('should successfully update an article by article_id', () => {
+    const updateObj = { inc_votes: 55 }
+    const expectedOutput = [{
+      article_id: 2,
+      title:'Sony Vaio; or, The Laptop',
+      topic: 'mitch',
+      author: 'icellusedkars',
+      body: 'Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.',
+      created_at: '2020-10-16 06:03:00Z',
+      votes: 55,
+      article_img_url: 'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700'
+    }]
+    return request(app)
+    .patch('/api/articles/2')
+    .send(updateObj)
+    .expect(200)
+    .then(({ body }) => {
+      expect(body.article).toEqual(expectedOutput);
+    })
+  })
+
+  // it('patch - 400 error', () => {
+
+  // })
+
+  // it('patch - 404 error', () => {
 
   // })
 
