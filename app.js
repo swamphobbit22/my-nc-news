10edea062f3f6c4884a0c7eb483887ab293c5a76
Kirsty <congokirsty@gmail.com>
2024-12-16 10:06:17 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const apiRouter = require('./routes/api.router');
 const { pgErrorhandler, customErrorhandler, serverErrorhandler } = require('./errors/error-handling');
 const { getWelcomeMsg } = require('./controllers/api-controller');
 
-
+app.use(cors());
 app.use(express.json());
 
 app.get('/', getWelcomeMsg);
