579ad91bd2e280ba22b3d6d52fadb2139e9bda41
Kirsty <congokirsty@gmail.com>
2024-12-19 14:14:36 +0000

    updates comments-model

--- db/test.sql
+++ db/test.sql
@@ -1,91 +1,99 @@
 \c nc_news_test
 
-SELECT * FROM articles
-WHERE article_id = 2;
+-- SELECT * FROM articles
+-- WHERE article_id = 2;
 
-\d articles
+-- \d articles
 
-\d comments
+-- \d comments
 
-        SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url,
-        COUNT(comments.comment_id) AS comment_count
-        FROM articles
-        LEFT JOIN comments
-        ON comments.article_id = articles.article_id
-        GROUP BY articles.article_id
-        ORDER BY articles.created_at DESC;
+--         SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url,
+--         COUNT(comments.comment_id) AS comment_count
+--         FROM articles
+--         LEFT JOIN comments
+--         ON comments.article_id = articles.article_id
+--         GROUP BY articles.article_id
+--         ORDER BY articles.created_at DESC;
 
-        SELECT * FROM comments;
+--         SELECT * FROM comments;
 
-        SELECT 
-        articles.author, 
-        articles.title, 
-        articles.article_id, 
-        articles.topic, 
-        articles.created_at, 
-        articles.votes, 
-        articles.article_img_url,
-        COUNT(comments.comment_id)::INTEGER AS comment_count
-        FROM articles
-        LEFT JOIN comments
-        ON comments.article_id = articles.article_id
-        GROUP BY articles.article_id
-        ORDER BY articles.created_at DESC;
+--         SELECT 
+--         articles.author, 
+--         articles.title, 
+--         articles.article_id, 
+--         articles.topic, 
+--         articles.created_at, 
+--         articles.votes, 
+--         articles.article_img_url,
+--         COUNT(comments.comment_id)::INTEGER AS comment_count
+--         FROM articles
+--         LEFT JOIN comments
+--         ON comments.article_id = articles.article_id
+--         GROUP BY articles.article_id
+--         ORDER BY articles.created_at DESC;
 
-        INSERT INTO comments 
-        (author, body, article_id)
-        VALUES ('lurker', 'This is a test comment, nothing special.', 5)
-        RETURNING *;
+--         INSERT INTO comments 
+--         (author, body, article_id)
+--         VALUES ('lurker', 'This is a test comment, nothing special.', 5)
+--         RETURNING *;
         
-        \d users
+--         \d users
   
-        \dt
+--         \dt
 
-        \d articles
-        \d comments
+--         \d articles
+--         \d comments
 
-        SELECT * FROM articles WHERE article_id = 5;
-        SELECT * FROM comments WHERE author = 'lurker';
-        SELECT * FROM articles WHERE article_id = 5;
+--         SELECT * FROM articles WHERE article_id = 5;
+--         SELECT * FROM comments WHERE author = 'lurker';
+--         SELECT * FROM articles WHERE article_id = 5;
 
-        SELECT * FROM articles;
+--         SELECT * FROM articles;
 
-        SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url,
-        COUNT(comments.comment_id) AS comment_count
-        FROM articles
-        LEFT JOIN comments
-        ON comments.article_id = articles.article_id
-        GROUP BY articles.article_id
-        ORDER BY articles.created_at DESC;
+--         SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url,
+--         COUNT(comments.comment_id) AS comment_count
+--         FROM articles
+--         LEFT JOIN comments
+--         ON comments.article_id = articles.article_id
+--         GROUP BY articles.article_id
+--         ORDER BY articles.created_at DESC;
 
-        -- UPDATE articles
-        -- SET votes = $1,
-        -- WHERE article_id = $2
+--         -- UPDATE articles
+--         -- SET votes = $1,
+--         -- WHERE article_id = $2
 
-        SELECT * FROM users;
+--         SELECT * FROM users;
 
-        \d topics
+--         \d topics
 
-        \d articles
+--         \d articles
 
-        SELECT 
-        articles.author, 
-        articles.title, 
-        articles.article_id, 
-        articles.topic, 
-        articles.created_at, 
-        articles.votes, 
-        articles.article_img_url,
-        COUNT(comments.comment_id)::INTEGER AS comment_count
-        FROM articles
-        LEFT JOIN comments
-        ON comments.article_id = articles.article_id
-        WHERE articles.topic = 'Bobcats' AND articles.topic IN (SELECT slug FROM topics)
-        GROUP BY articles.article_id
-        ORDER BY articles.created_at;
+--         SELECT 
+--         articles.author, 
+--         articles.title, 
+--         articles.article_id, 
+--         articles.topic, 
+--         articles.created_at, 
+--         articles.votes, 
+--         articles.article_img_url,
+--         COUNT(comments.comment_id)::INTEGER AS comment_count
+--         FROM articles
+--         LEFT JOIN comments
+--         ON comments.article_id = articles.article_id
+--         WHERE articles.topic = 'Bobcats' AND articles.topic IN (SELECT slug FROM topics)
+--         GROUP BY articles.article_id
+--         ORDER BY articles.created_at;
 
-\d comments
+-- \d comments
 
-SELECT * FROM comments;
+-- SELECT * FROM comments;
 
-SELECT * FROM articles;
+-- SELECT * FROM articles;
+
+--         SELECT * FROM comments
+--         WHERE author = 'grumpy19'
+--         ORDER BY created_at DESC;
+
+        SELECT * FROM comments 
+        WHERE author = 'lurker'
+        ORDER BY created_at DESC;
--- models/comments-model.js
+++ models/comments-model.js
@@ -38,6 +38,7 @@ exports.deleteComment = ({comment_id}) => {
     return db.query(`
         DELETE FROM comments
         WHERE comment_id = $1`, [comment_id])
+
 }
 
 exports.selectCommentsByUsername =({username}) => {
@@ -46,6 +47,12 @@ exports.selectCommentsByUsername =({username}) => {
         WHERE author = $1
         ORDER BY created_at DESC
         `, [username])
+        .then(({ rows }) => {
+            if (rows.length === 0) {
+                return Promise.reject({ status: 404, msg: 'comment does not exist'})
+            }
+            return rows;
+        })
 }
 
 exports.updateCommentVotes = (inc_votes, comment_id) => {
